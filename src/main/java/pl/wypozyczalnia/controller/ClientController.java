15c5257ae1a06771e22f8101c171c1fb11813828
Dawid Kamasz <dawid.kamasz@gmail.com>
2019-08-04 09:55:05 +0200

    walidacja rejestracji

--- src/main/java/pl/wypozyczalnia/controller/ClientController.java
+++ src/main/java/pl/wypozyczalnia/controller/ClientController.java
@@ -2,12 +2,14 @@ package pl.wypozyczalnia.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.wypozyczalnia.model.Client;
 import pl.wypozyczalnia.service.ClientService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -27,12 +29,15 @@ public class ClientController {
     }
 
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute Client client){
+    public String registerUser(@ModelAttribute @Valid Client client, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "registerClient";
+        }
         List<Client> clientList = clientService.findAllByEmail(client.getEmail());
         if (clientList.isEmpty()){
         clientService.saveClient(client);
-        return "registerClient";
+        return "done";
         }
-        return "redirect:/";
+        return "error";
     }
 }
